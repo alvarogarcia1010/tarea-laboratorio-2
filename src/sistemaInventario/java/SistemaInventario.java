da45815ca9890cf25d6ae544dd371bfc3cd43e01
alvarogarcia1010 <00111317@uca.edu.sv>
2018-04-05 23:20:31 -0600

    Avance

---
+++ src/sistemaInventario/java/SistemaInventario.java
@@ -0,0 +1,14 @@
+
+package sistemaInventario.java;
+
+/**
+ * @author Alvaro García <alvarogarcia1010 at github.com>
+ */
+
+public class SistemaInventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
